f6e273d69e618ad594441e47aa6976949493f3ad
Louis Moureaux <m_louis30@yahoo.com>
2020-08-29 20:05:40 +0200

    Remove the Category thing

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from cms.extensions import PageExtensionAdmin
-
-from .models import CategoryExtension
-
-class CategoryExtensionAdmin(PageExtensionAdmin):
-    pass
-
-admin.site.register(CategoryExtension, CategoryExtensionAdmin)
